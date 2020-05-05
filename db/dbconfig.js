c1d68a8a809ac78a0481274adc2394ee39413e7c
rhensen <ruben.hensen@gmail.com>
2020-05-05 17:07:53 +0200

    Changing environment

--- db/dbconfig.js
+++
@@ -1,9 +0,0 @@
-
-  db.auth('root', 'example');
-
-  db = db.getSiblingDB('akela');
-
-  db.createUser({
-    user: 'api-user',
-    pwd: 'api-password',
-    roles: [ { role: 'root', db: 'admin', } ]});
