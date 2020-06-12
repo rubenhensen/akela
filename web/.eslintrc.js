f06bc9fb553142f52897c9820f7f287ae103400f
Ruben Hensen <ruben.hensen@gmail.com>
2020-06-12 22:28:13 +0200

    Add airbnb eslint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,13 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true,
-        "node": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "rules": {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
     }
+  ],
 };
