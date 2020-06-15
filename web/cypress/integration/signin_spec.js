4372c5808afd604b8a9a58768176103d5ec00ec2
Ruben Hensen <ruben.hensen@gmail.com>
2020-06-15 21:34:03 +0200

    Add wait to signin test

--- web/cypress/integration/signin_spec.js
+++ web/cypress/integration/signin_spec.js
@@ -1,16 +1,17 @@
-describe('Sign in page', () => {
-	beforeEach(() => {
-		cy.visit('/signin')
-	});
+describe("Sign in page", () => {
+  beforeEach(() => {
+    cy.visit("/signin");
+  });
 
-	it('has the correct <h2>', () => {
-		cy.contains('h2', 'Sign in')
-	});
+  it("has the correct <h2>", () => {
+    cy.contains("h2", "Sign in");
+  });
 
-	it('can log in', () => {
-		cy.get('#email').type('karel@gmail.com');
-		cy.get('#password').type('karel');
-		cy.get('#submit').click();
-		cy.url().should('include', '/');
-	});
+  it("can log in", () => {
+    cy.wait(1000);
+    cy.get("#email").type("karel@gmail.com");
+    cy.get("#password").type("karel");
+    cy.get("#submit").click();
+    cy.url().should("include", "/");
+  });
 });
