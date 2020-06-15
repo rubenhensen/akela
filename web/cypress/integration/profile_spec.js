388008fe134bd1e720ff4b2ff2fcf4f40c50608a
Ruben Hensen <ruben.hensen@gmail.com>
2020-06-15 21:23:27 +0200

    Fix profile_spec test

--- web/cypress/integration/profile_spec.js
+++ web/cypress/integration/profile_spec.js
@@ -1,19 +1,18 @@
-describe('Profile page', () => {
-	beforeEach(() => {
-		cy.visit('/signin')
-		cy.wait(600);
-		cy.get('#email').type('karel@gmail.com');
-		cy.get('#password').type('karel');
-		cy.get('#submit').click();
-		cy.contains('account_box').click();
-	});
+describe("Profile page", () => {
+  beforeEach(() => {
+    cy.visit("/signin");
+    cy.wait(600);
+    cy.get("#email").type("karel@gmail.com");
+    cy.get("#password").type("karel");
+    cy.get("#submit").click();
+    cy.contains("account_box").click();
+  });
 
-	it('can navigate to the attendance page', () => {
-		cy.url().should('include', '/profile');
-	});
+  it("can navigate to the attendance page", () => {
+    cy.url().should("include", "/profile");
+  });
 
-	it('has the correct <h3> headers', () => {
-		cy.contains('Naam');
-	});
+  it("has the correct <h3> headers", () => {
+    cy.contains("Other users");
+  });
 });
-
